7ecd1e3ff6c3b8f0eeb48b927d51505e0e33f3a2
ando <ando.art@qq.com>
2023-04-11 11:48:07 +0800

    添加注释

--- app/src/main/java/com/ando/chathouse/model/ChatModel.kt
+++ app/src/main/java/com/ando/chathouse/model/ChatModel.kt
@@ -4,6 +4,13 @@ import kotlinx.coroutines.flow.Flow
 
 interface ChatModel {
     val label:String
+
+    /**
+     * 发送消息到ai模型
+     * @param message: 发送到模型的消息
+     * @param para: 模型参数
+     * @return ai模型响应的消息
+     */
     fun sendMessage(message:String,para: Para?):Flow<String?>
     data class Para(val apiKey: String?)
 }
--- app/src/main/java/com/ando/chathouse/model/LongChatModel.kt
+++ app/src/main/java/com/ando/chathouse/model/LongChatModel.kt
@@ -4,6 +4,12 @@ import com.ando.chathouse.domain.pojo.RoleMessage
 import kotlinx.coroutines.flow.Flow
 
 interface LongChatModel : ChatModel {
+    /**
+     * 发送消息到ai模型
+     * @param messages: 发送到模型的消息
+     * @param para: 模型参数
+     * @return ai模型响应的消息
+     */
     fun sendMessages(messages: List<RoleMessage>, para: ChatModel.Para?=null): Flow<String?>
 
     override fun sendMessage(message: String, para: ChatModel.Para?): Flow<String?> {
